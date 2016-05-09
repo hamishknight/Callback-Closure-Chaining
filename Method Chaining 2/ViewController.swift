d84d0837781885f7f229522dd7f4cdebc12a61f0
Hamish Knight <hk@southerncare.co.uk>
2016-05-09 13:39:00 +0100

    Initial Commit

---
+++ Method Chaining 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Method Chaining 2
+//
+//  Created by Hamish Knight on 09/05/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
